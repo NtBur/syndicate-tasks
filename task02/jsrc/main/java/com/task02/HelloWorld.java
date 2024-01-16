cffdec65156d58b43c2b413640a7baf2bf717c93
NtBur <ntbur8@gmail.com>
2024-01-16 22:05:56 +0200

    added task2-2

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,32 +2,31 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true
+        roleName = "hello_world-role",
+        isPublishVersion = true
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
-	private static final int SC_OK = 200;
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final int SC_OK = 200;
     private static final int SC_BAD_REQUEST = 400;
-    private final Gson gson = new Gson();
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
         context.getLogger().log(apiGatewayProxyRequestEvent.toString());
-        Map<String, String> queryStringParameters = apiGatewayProxyRequestEvent.getQueryStringParameters();
         try {
             return new APIGatewayProxyResponseEvent()
                     .withStatusCode(SC_OK)
-                    .withBody(gson.toJson("Hello from Lambda"));
+                    .withBody("Hello from Lambda");
         } catch (IllegalArgumentException exception) {
             return new APIGatewayProxyResponseEvent()
                     .withStatusCode(SC_BAD_REQUEST)
-                    .withBody(gson.toJson(exception.getMessage()));
+                    .withBody(exception.getMessage());
         }
     }
 }
