a20e05e60b43b03598eed0792d710c79b8c46353
NtBur <ntbur8@gmail.com>
2024-01-22 22:36:21 +0200

    added task04_3

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,23 +2,32 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(lambdaName = "sns_handler",
 	roleName = "sns_handler-role"
 )
 @SnsEventSource(targetTopic = "lambda_topic")
-public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+public class SnsHandler implements RequestHandler<SNSEvent, Void> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public Void handleRequest(SNSEvent snsEvent, Context context) {
+		for (SNSEvent.SNSRecord sns : snsEvent.getRecords()) {
+			processMessage(sns, context);
+		}
+		context.getLogger().log("done");
+		return null;
+	}
+
+	private void processMessage(SNSEvent.SNSRecord sns, Context context) {
+		try {
+			context.getLogger().log(sns.getSNS().getMessage());
+		} catch (Exception e) {
+			context.getLogger().log("An error occurred");
+			throw e;
+		}
 	}
 }
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,23 +2,32 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(lambdaName = "sqs_handler",
 	roleName = "sqs_handler-role"
 )
 @SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 10)
-public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public Void handleRequest(SQSEvent sqsEvent, Context context) {
+		for (SQSMessage msg : sqsEvent.getRecords()) {
+			processMessage(msg, context);
+		}
+		context.getLogger().log("done");
+		return null;
+	}
+
+	private void processMessage(SQSMessage msg, Context context) {
+		try {
+			context.getLogger().log(msg.getBody());
+		} catch (Exception e) {
+			context.getLogger().log("An error occurred");
+			throw e;
+		}
 	}
 }
