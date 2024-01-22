05393792acf8defc77aec17cb21cef58def3866b
NtBur <ntbur8@gmail.com>
2024-01-22 18:14:34 +0200

    added task04_1

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,11 +2,13 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@SnsEventSource(targetTopic = "Lambda_topic")
 @LambdaHandler(lambdaName = "sns_handler",
 	roleName = "sns_handler-role"
 )
