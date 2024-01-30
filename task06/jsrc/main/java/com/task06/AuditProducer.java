f97d483a275c85d72b8f8646873566074209c996
NtBur <ntbur8@gmail.com>
2024-01-30 18:40:17 +0200

    added task06_1

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -1,12 +1,13 @@
 package com.task06;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
@@ -25,41 +26,54 @@ import java.util.UUID;
         @EnvironmentVariable(key = "target_table", value = "${target_table}")
 })
 @DynamoDbTriggerEventSource(targetTable = "Configuration", batchSize = 1)
-public class AuditProducer implements RequestHandler<DynamodbEvent, Map<String, AttributeValue>> {
+public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
     private AmazonDynamoDB amazonDynamoDB;
 
-    public Map<String, AttributeValue> handleRequest(DynamodbEvent dynamodbEvent, Context context) {
+    public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
         this.initDynamoDbClient();
-        for (DynamodbEvent.DynamodbStreamRecord record : dynamodbEvent.getRecords()) {
-            if (record != null) {
-               return createAudit(record);
+        for (DynamodbStreamRecord record : dynamodbEvent.getRecords()) {
+
+            if (record.getEventName().equals("INSERT")) {
+                updateAuditForInsert(record);
+            } else if (record.getEventName().equals("MODIFY")) {
+                updateAuditForModify(record);
             }
         }
+
         return null;
     }
 
-    private Map<String, AttributeValue> createAudit(DynamodbEvent.DynamodbStreamRecord record) throws ConditionalCheckFailedException {
-
+    private Map<String, AttributeValue> updateAuditForInsert(DynamodbStreamRecord record) throws ConditionalCheckFailedException {
         Map<String, AttributeValue> attributesMap = new HashMap<>();
-        Map<String, com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue> oldTable = record.getDynamodb().getOldImage();
-        Map<String, com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue> newTable = record.getDynamodb().getOldImage();
+        Map<String, com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue> newTable = record.getDynamodb().getNewImage();
 
         attributesMap.put("id", new AttributeValue(String.valueOf(UUID.randomUUID())));
-        attributesMap.put("itemKey", new AttributeValue(String.valueOf(oldTable.get("key"))));
+        attributesMap.put("itemKey", new AttributeValue(String.valueOf(newTable.get("key"))));
         attributesMap.put("modificationTime", new AttributeValue(String.valueOf(Instant.now())));
-        if (!oldTable.get("value").equals(newTable.get("value"))) {
-            attributesMap.put("updatedAttribute", new AttributeValue("value"));
-            attributesMap.put("oldValue", new AttributeValue(String.valueOf(oldTable.get("value"))));
-            attributesMap.put("newValue", new AttributeValue(String.valueOf(newTable.get("value"))));
-        } else {
-            Map<String, AttributeValue> newValues = new HashMap<>();
-            newValues.put("key", new AttributeValue(String.valueOf(newTable.get("key"))));
-            newValues.put("value", new AttributeValue(String.valueOf(newTable.get("value"))));
-            attributesMap.put("newValue", new AttributeValue().withM(newValues));
-        }
+        Map<String, AttributeValue> newValues = new HashMap<>();
+        newValues.put("key", new AttributeValue(String.valueOf(newTable.get("key"))));
+        newValues.put("value", new AttributeValue(String.valueOf(newTable.get("value"))));
+        attributesMap.put("newValue", new AttributeValue().withM(newValues));
+
         amazonDynamoDB.putItem(System.getenv("target_table"), attributesMap);
         return attributesMap;
+    }
 
+    private Map<String, AttributeValue> updateAuditForModify(DynamodbStreamRecord record) throws ConditionalCheckFailedException {
+        Map<String, AttributeValue> attributesMap = new HashMap<>();
+        Map<String, com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue> oldTable = record.getDynamodb().getOldImage();
+        Map<String, com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue> newTable = record.getDynamodb().getNewImage();
+
+        attributesMap.put("id", new AttributeValue(String.valueOf(UUID.randomUUID())));
+        attributesMap.put("itemKey", new AttributeValue(String.valueOf(newTable.get("key"))));
+        attributesMap.put("modificationTime", new AttributeValue(String.valueOf(Instant.now())));
+
+        attributesMap.put("updatedAttribute", new AttributeValue("value"));
+        attributesMap.put("oldValue", new AttributeValue(String.valueOf(oldTable.get("value"))));
+        attributesMap.put("newValue", new AttributeValue(String.valueOf(newTable.get("value"))));
+
+        amazonDynamoDB.putItem(System.getenv("target_table"), attributesMap);
+        return attributesMap;
     }
 
     private void initDynamoDbClient() {
