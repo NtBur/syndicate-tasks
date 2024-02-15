bf988531969a0afbea7dc1922d42c82557db7437
NtBur <ntbur8@gmail.com>
2024-02-15 18:51:32 +0200

    added task08_2

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -7,6 +7,9 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task08.response.ErrorMessage;
@@ -17,7 +20,13 @@ import java.util.Map;
 
 
 @LambdaHandler(lambdaName = "api_handler",
-		roleName = "api_handler-role"
+		roleName = "api_handler-role",
+		layers = {"sdk-layer"}
+)
+@LambdaLayer(
+		layerName = "sdk-layer",
+		runtime = DeploymentRuntime.JAVA8,
+		artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
