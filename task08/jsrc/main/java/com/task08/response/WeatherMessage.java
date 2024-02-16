97578fb46cf99331e28c55549fa9198aa2940764
NtBur <ntbur8@gmail.com>
2024-02-17 01:08:52 +0200

    added task08_12

--- task08/jsrc/main/java/com/task08/response/WeatherMessage.java
+++ task08/jsrc/main/java/com/task08/response/WeatherMessage.java
@@ -7,27 +7,9 @@ import java.net.URL;
 
 
 public class WeatherMessage {
-    private final String BASE_URL = "https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238&hourly=temperature_2m&timezone=Europe%2FKyiv";
-    public StringBuffer getWeather() {
-
-        StringBuffer response = null;
-        try {
-            URL url = new URL(BASE_URL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            response = new StringBuffer();
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-            }
-            rd.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        assert response != null;
-        return response;
+    private final String message;
+    public WeatherMessage(String message) {
+        this.message = message;
     }
+
 }
