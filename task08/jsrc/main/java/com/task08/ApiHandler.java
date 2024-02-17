27db2d24644082777533895b42f3d58085901c6d
NtBur <ntbur8@gmail.com>
2024-02-17 09:13:21 +0200

    added task08_13

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -15,10 +15,7 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task08.response.ErrorMessage;
 import com.task08.response.WeatherMessage;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 
 @LambdaHandler(lambdaName = "api_handler",
@@ -39,40 +36,20 @@ public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, A
 	private static final int SC_OK = 200;
 	private static final int SC_BAD_REQUEST = 400;
 	private final Gson gson = new Gson();
+
 	private final String BASE_URL = "https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238&hourly=temperature_2m&timezone=Europe%2FKyiv";
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
 		context.getLogger().log(apiGatewayProxyRequestEvent.toString());
+		String url = gson.toJson(BASE_URL);
 		try {
-			return new APIGatewayProxyResponseEvent()
+			return (url!=null)?new APIGatewayProxyResponseEvent()
 					.withStatusCode(SC_OK)
-					.withBody(gson.toJson(new WeatherMessage(getWeather())));
+					.withBody(new WeatherMessage().getWeather()):null;
 		} catch (IllegalArgumentException exception) {
 			return new APIGatewayProxyResponseEvent()
 					.withStatusCode(SC_BAD_REQUEST)
 					.withBody(gson.toJson(new ErrorMessage(exception.getMessage())));
 		}
-	}
-	public String getWeather() {
-
-		StringBuffer response = null;
-		try {
-			URL url = new URL(BASE_URL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String line;
-			response = new StringBuffer();
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-			}
-			rd.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		assert response != null;
-		return response.toString();
 	}
 }
