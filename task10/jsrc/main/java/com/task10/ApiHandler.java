f13d35dc9374a5a91a2a878d64f37fe892d88939
NtBur <ntbur8@gmail.com>
2024-02-21 00:10:19 +0200

    added task10_2

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.CloudFrontEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -13,12 +14,17 @@ import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import software.amazon.awssdk.core.Response;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
 
 
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "api_handler",
@@ -34,99 +40,120 @@ import java.util.Map;
 		@EnvironmentVariable(key = "reservations_table", value = "${reservations_table}"),
 		@EnvironmentVariable(key = "booking_userpool", value = "${booking_userpool}")
 })
-public class ApiHandler implements  RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 	private CognitoIdentityProviderClient cognitoClient = CognitoIdentityProviderClient.builder()
 			.region(Region.of(System.getenv("region")))
 			.build();
-		public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+		public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+			String path = input.getPath();
 
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			Map<String, String> requestBody = gson.fromJson(event.getBody(), new TypeToken<Map<String, String>>() {}.getType());
-
-			String firstName = requestBody.get("firstName");
-			String lastName = requestBody.get("lastName");
-			String email = requestBody.get("email");
-			String password = requestBody.get("password");
-
-			if ("/signup".equals(event.getResource()) && "POST".equals(event.getHttpMethod())) {
-				AdminCreateUserRequest signUpRequest = AdminCreateUserRequest.builder()
-						.userPoolId(System.getenv("booking_userpool"))
-						.username(email)
-						.userAttributes(
-								AttributeType.builder().name("given_name").value(firstName).build(),
-								AttributeType.builder().name("family_name").value(lastName).build(),
-								AttributeType.builder().name("email").value(email).build()
-						)
-						.temporaryPassword("TempPassword1!")
-						.build();
-				try {
-					AdminCreateUserResponse signUpResponse = cognitoClient.adminCreateUser(signUpRequest);
-					return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("User registered successfully");
-				} catch (UsernameExistsException e) {
-					return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Username already exists");
-				} catch (CognitoIdentityProviderException e) {
-					return new APIGatewayProxyResponseEvent().withStatusCode(e.statusCode()).withBody(e.awsErrorDetails().errorMessage());
-			/*	try {
-					AdminCreateUserResponse response = cognitoClient.adminCreateUser(signUpRequest);
-					if(response.sdkHttpResponse().isSuccessful()) {
-						// If user creation is successful, set the real (given) password
-						AdminSetUserPasswordRequest setPasswordRequest = AdminSetUserPasswordRequest.builder()
-								.userPoolId(System.getenv("booking_userpool"))
-								.username(email)
-								.password(password)
-								.permanent(true)
-								.build();
-
-						// Use the client to send the request
-						AdminSetUserPasswordResponse setPasswordResponse = cognitoClient.adminSetUserPassword(setPasswordRequest);
-						if (setPasswordResponse.sdkHttpResponse().isSuccessful()) {
-							return new APIGatewayProxyResponseEvent().withStatusCode(200);
-						}
+			switch (path) {
+				case "/signup":
+					return handleSignup(input);
+				case "/signin":
+					return handleSignin(input);
+				case "/tables":
+					String httpMethod = input.getHttpMethod();
+					if ("GET".equalsIgnoreCase(httpMethod)) {
+						return handleGetTables(input);
+					} else if ("POST".equalsIgnoreCase(httpMethod)) {
+						//return handlePostTables(input);
 					}
-				} catch (Exception ex) {
-					return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody(ex.getMessage());*/
+					break;
+				case "/tables/{tableId}":
+				//	return handleGetTable(input);
+			}
+			return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Invalid request");
+		}
+
+		private APIGatewayProxyResponseEvent handleSignup(APIGatewayProxyRequestEvent inputBody) {
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			Map<String, String> input = gson.fromJson(inputBody.getBody(), new TypeToken<Map<String, String>>() {}.getType());
+
+					AdminCreateUserRequest adminCreateUserRequest = AdminCreateUserRequest.builder()
+							.userPoolId(System.getenv("booking_userpool"))
+							.messageAction("SUPPRESS")
+							.username(input.get("email"))
+							.temporaryPassword(input.get("password"))
+							.userAttributes(
+									AttributeType.builder().name("name").value(input.get("firstName")).build(),
+									AttributeType.builder().name("family_name").value(input.get("lastName")).build(),
+									AttributeType.builder().name("email").value(input.get("email")).build()
+							)
+							.build();
+
+					try {
+						AdminCreateUserResponse adminCreateUserResponse = cognitoClient.adminCreateUser(adminCreateUserRequest);
+						return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("User registered successfully");
+					} catch (UsernameExistsException e) {
+						return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Username already exists");
+					}
 				}
-			} else if ("/signin".equals(event.getResource()) && "POST".equals(event.getHttpMethod())) {
 
 
 
-				AdminInitiateAuthRequest signInRequest = AdminInitiateAuthRequest.builder()
-						.authFlow(AuthFlowType.ADMIN_USER_PASSWORD_AUTH)
-						.clientId(getClientId(cognitoClient))
-						.userPoolId(System.getenv("booking_userpool"))
-						.authParameters(new HashMap<String, String>() {{
-							put("email", email);
-							put("password", password);
-						}})
-						.build();
-				try {
-					AdminInitiateAuthResponse result = cognitoClient.adminInitiateAuth(signInRequest);
-					String accessToken = result.authenticationResult().idToken();
+		private APIGatewayProxyResponseEvent handleSignin(APIGatewayProxyRequestEvent inputBody) {
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			Map<String, String> input = gson.fromJson(inputBody.getBody(), new TypeToken<Map<String, String>>() {}.getType());
+			InitiateAuthRequest authRequest = InitiateAuthRequest.builder()
+					.authFlow(AuthFlowType.USER_PASSWORD_AUTH)
+					.clientId("clientId")
+					.authParameters(buildAuthParameters(input.get("email"), input.get("password")))
+					.build();
 
-					return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("{\"accessToken\":\"" + accessToken + "\"}");
-				} catch (Exception e) {
-					return new APIGatewayProxyResponseEvent().withStatusCode(403).withBody("{\"message\":\"Access denied: " + e.getMessage() +  "\"}");
+			try {
+				InitiateAuthResponse authResponse = cognitoClient.initiateAuth(authRequest);
+				String accessToken = authResponse.authenticationResult().idToken();
+				return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("{\"accessToken\":\"" + accessToken + "\"}");
+			} catch (Exception e) {
+				return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Auth failed: " + e.getMessage());
+			}
+		}
+
+	private Map<String,String> buildAuthParameters(String username, String password) {
+		Map<String,String> authParameters = new HashMap<>();
+		authParameters.put("USERNAME", username);
+		authParameters.put("PASSWORD", password);
+		return authParameters;
+	}
+
+
+		private APIGatewayProxyResponseEvent handleGetTables(APIGatewayProxyRequestEvent input) {
+			String accessToken = null;
+			Map<String, String> headers = input.getHeaders();
+			if (headers.containsKey("Authorization")) {
+				String authHeader = headers.get("Authorization");
+				if (authHeader.startsWith("Bearer ")) {
+					accessToken = authHeader.substring("Bearer ".length());
 				}
 			}
-			return new APIGatewayProxyResponseEvent()
-					.withStatusCode(200)
-					.withBody("Valid resource or method.");
+
+			if (accessToken == null || accessToken.isEmpty()) {
+				return new APIGatewayProxyResponseEvent().withStatusCode(403).withBody("No Authorization token detected.");
+			}
+			List<Map<String, Object>> tables = new ArrayList<>();
+
+			Map<String, Object> table1 = new HashMap<>();
+			table1.put("id", 1);
+			table1.put("number", 10);
+			table1.put("places", 4);
+			table1.put("isVip", false);
+			table1.put("minOrder", 20);
+			tables.add(table1);
+
+			Map<String, Object> table2 = new HashMap<>();
+			table2.put("id", 2);
+			table2.put("number", 20);
+			table2.put("places", 2);
+			table2.put("isVip", true);
+			tables.add(table2);
+
+			Map<String, List<Map<String, Object>>> response = new HashMap<>();
+			response.put("tables", tables);
+
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			String json = gson.toJson(response);
+			return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(json);
 		}
 
-	private String getClientId(CognitoIdentityProviderClient cognitoClient) {
-		ListUserPoolClientsRequest listUserPoolClientsRequest = ListUserPoolClientsRequest.builder()
-				.userPoolId(System.getenv("booking_userpool"))
-				.maxResults(60)
-				.build();
-
-		ListUserPoolClientsResponse response = cognitoClient.listUserPoolClients(listUserPoolClientsRequest);
-
-		if (!response.userPoolClients().isEmpty()) {
-			return response.userPoolClients().get(0).clientId();
-		}
-		return null;
-	}
-
-
-	}
+}
